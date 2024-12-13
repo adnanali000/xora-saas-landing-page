4b1263ef70090219b53a5338829d6baf7aa4c195
adnanmusil <adnan.museel123@gmail.com>
2024-12-13 17:53:06 +0500

    hero section , button and marker component

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link as LinkScroll } from 'react-scroll'
 import clsx from "clsx";
 
@@ -11,7 +11,7 @@ const NavLink = ({ title }) => (
 )
 
 function Header() {
-    const [isOpen,setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <header className='fixed top-0 left-0 z-50 w-full py-10'>
@@ -21,9 +21,9 @@ function Header() {
                 </a>
 
                 <div className={clsx(
-            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
-            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
-          )}>
+                    "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+                    isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none",
+                )}>
                     <div className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4'>
                         <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
                             <ul className='flex max-lg:block max-lg:px-12'>
@@ -55,7 +55,7 @@ function Header() {
                                 alt='outline'
                                 className='relative z-2'
                             />
-                             <img
+                            <img
                                 src="/images/bg-outlines-fill.png"
                                 width={960}
                                 height={380}
@@ -66,8 +66,8 @@ function Header() {
                     </div>
                 </div>
 
-                <button onClick={()=>setIsOpen(prev => !prev)} className='lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center'>
-                    <img src={`/images/${isOpen ? 'close':'magic'}.svg`} alt='magic' className='size-1/2 object-contain' />
+                <button onClick={() => setIsOpen(prev => !prev)} className='lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center'>
+                    <img src={`/images/${isOpen ? 'close' : 'magic'}.svg`} alt='magic' className='size-1/2 object-contain' />
                 </button>
 
             </div>
